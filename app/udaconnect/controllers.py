1303776649385510533071e161fb06f9141cb925
Pasch94 <paul.schmidl@yahoo.de>
2021-08-09 20:58:25 +0200

    Initial commit of everything

---
+++ app/udaconnect/controllers.py
@@ -0,0 +1,68 @@
+import grpc
+import json
+import os
+
+from datetime import datetime
+
+from app.udaconnect.models import Person
+from app.udaconnect.schemas import (
+    PersonSchema,
+)
+from app.udaconnect.proto.person_pb2 import PersonRequest, PersonListRequest
+from app.udaconnect.proto.person_pb2_grpc import PersonServiceStub
+from flask import request, Response
+from flask_accepts import accepts, responds
+from flask_restx import Namespace, Resource
+from typing import Optional, List
+
+DATE_FORMAT = "%Y-%m-%d"
+
+api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
+
+TOPIC_NAME = os.getenv('KAFKA_PERSON_TOPIC', 'person')
+KAFKA_HOST = os.getenv('KAFKA_HOST', '192.168.65.4')
+KAFKA_PORT = os.getenv('KAFKA_PORT', '9092')
+KAFKA_SERVER = ':'.join([KAFKA_HOST, KAFKA_PORT])
+kafka_producer = KafkaProducer(
+        bootstrap_servers=KAFKA_SERVER,
+        api_version=(2, 8, 0),
+        client_id=__name__,
+        value_serializer=lambda m: json.dumps(m).encode('utf-8'))
+
+GRPC_PORT = os.getenv('GRPC_PORT', '5005')
+GRPC_HOST = os.getenv('GRPC_HOST', 'localhost')
+
+print(':'.join([GRPC_HOST, GRPC_PORT]))
+GRPC_CHANNEL = grpc.insecure_channel(':'.join([GRPC_HOST, GRPC_PORT]), options=(('grpc.enable_http_proxy', 0),))
+grpc_stub = PersonServiceStub(GRPC_CHANNEL)
+
+
+@api.route("/persons")
+class PersonsResource(Resource):
+    @accepts(schema=PersonSchema)
+    @responds(schema=PersonSchema)
+    def post(self) -> Person:
+        kafka_data = request.get_json()
+        kafka_producer.send(TOPIC_NAME, kafka_data)
+        return Response(status=202) 
+
+    @responds(schema=PersonSchema, many=True)
+    def get(self) -> List[Person]:
+        person_list = grpc_stub.GetAll(PersonListRequest())
+        # TODO need to rearrange data here?
+        # Lets check the data:
+        print(person_list)
+        return person_list 
+
+
+@api.route("/persons/<person_id>")
+@api.param("person_id", "Unique ID for a given Person", _in="query")
+class PersonResource(Resource):
+    @responds(schema=PersonSchema)
+    def get(self, person_id) -> Person:
+        person = grpc_stub.Get(PersonRequest(id=int(person_id)))
+        print("Got grpc response: {}".format(person))
+        # Maybe need to reformat / assign here
+        return person
+
+
